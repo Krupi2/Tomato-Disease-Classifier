8abad1bc370d36f678ea37a063c32edd43d76401
mkrupa <162881340+Krupi2@users.noreply.github.com>
2026-06-16 08:14:39 +0200

    Poprawki dokeryzacji
    Podczas dokeryzacji nazwa kontenera była losowa co utrudniło zlokalizowanie aplikacji oraz naprawiono błędy z wersjami pakietów w requirements.txt

--- run.bat
+++ run.bat
@@ -4,5 +4,5 @@ echo Uruchamianie kontenera tomato-ai...
 echo Aplikacja bedzie dostepna pod adresem: http://127.0.0.1:8000
 echo Naciśnij CTRL+C, aby zatrzymac serwer.
 echo =========================================
-docker run -p 8000:8000 tomato-ai
+docker run --name tomato-ai-app --rm -p 8000:8000 tomato-ai
 pause
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ echo "Uruchamianie kontenera tomato-ai..."
 echo "Aplikacja będzie dostępna pod adresem: http://127.0.0.1:8000"
 echo "Naciśnij CTRL+C, aby zatrzymać serwer."
 echo "========================================="
-docker run -p 8000:8000 tomato-ai
+docker run --name tomato-ai-app --rm -p 8000:8000 tomato-ai
